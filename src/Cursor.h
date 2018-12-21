d2c2350d80c47ab398e1d49b58a5227f00fb2862
ye-tao <yelt@live.cn>
2018-12-21 21:30:54 +0800

    清理无用的文件

--- src/Cursor.h
+++ src/Cursor.h
@@ -4,3 +4,10 @@
 #define MOVEDOWN(x)  do{ if((x) != 0) printf("\e[%dB", (x)); }
 #define MOVERIGHT(x) do{ if((x) != 0) printf("\e[%dC", (x)); }
 #define MOVELEFT(x)  do{ if((x) != 0) printf("\e[%dD", (x)); }
+
+#define FORECOLOR_BLACK  30
+#define FORECOLOR_RED    31
+#define FORECOLOR_GREEN  32
+#define FORECOLOR_YELLOW 33
+
+
